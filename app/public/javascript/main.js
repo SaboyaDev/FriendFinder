5cf707f416fbc04ca50af700373d5660a2e69ac2
joseluissaboya <joseluissaboya@gmail.com>
2019-03-04 04:05:00 -0500

    Fixed some Questions, improved my main.js file, fixed sticky footer on home page, increase the area where poeple can click for eah question.

--- app/public/javascript/main.js
+++ app/public/javascript/main.js
@@ -1,47 +1,52 @@
 $(".submitBtn").on("click", function (event) {
-
   event.preventDefault();
+  collectUserInput(scores);
+  clearValueInputs();
+});
 
+function collectUserInput(scores) {
+  var scores = [];
+  for (var i = 1; i <= 10; i++) {
+    scores.push($("#q" + i).val())
+  }
   var userData = {
     name: $("#name").val(),
     photo: $("#img").val(),
-    scores: [
-      $("#q1").val(),
-      $("#q2").val(),
-      $("#q3").val(),
-      $("#q4").val(),
-      $("#q5").val(),
-      $("#q6").val(),
-      $("#q7").val(),
-      $("#q8").val(),
-      $("#q9").val(),
-      $("#q10").val()
-    ]
+    scores: scores
   };
-  console.log(userData);
+  postUserData(userData);
+}
 
+function postUserData(userData) {
   // AJAX post the data to the friends API.
   $.post("/api/friends", userData, function (data) {
-
-    console.log(data);
     // Grab the result from the AJAX post so that the best match's name and photo are displayed.
     $("#match-name").text(data.name);
     $("#match-img").attr("src", data.photo);
     for (var i = 0; i < data.length; i++) {
-      console.log(data[i].name);
       var matchName = data[i].name;
       var matchPhoto = data[i].photo;
-      console.log(matchPhoto);
-
-      $(".modal-body").append("<p>" + matchName + "</p>");
-      $(".modal-body").append($("<img>").attr("src", matchPhoto).css({
-        height: "150px",
-        width: "100px",
-        "margin-bottom": "30px" 
-      }));
+      // building the modal
+      $(".modal-body")
+        .append("<p>" + matchName + "</p>");
+      $(".modal-body")
+        .append($("<img>")
+          .attr("src", matchPhoto)
+          .css({
+            height: "150px",
+            width: "100px",
+            "margin-bottom": "30px"
+          }));
+      // displays the results in the modal
       $("#show-results-modal").modal("toggle");
-      $("#name").val("");
-      $("#img").val("");
     }
   });
-});
+}
+
+function clearValueInputs() {
+  $("#name").val("");
+  $("#img").val("");
+  for (var i = 1; i <= 10; i++) {
+    $("#q" + i).val("");
+  }
+}
